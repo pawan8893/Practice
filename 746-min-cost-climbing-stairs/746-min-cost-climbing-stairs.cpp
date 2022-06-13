0b1db8ed469d8e23ce2ae16db783be95e2e97ee7
Pawan kumar sah <53905882+pawan8893@users.noreply.github.com>
2022-06-14 02:17:44 +0530

    Time: 3 ms (96.59%), Space: 13.6 MB (78.44%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -41,12 +41,24 @@ class Solution {
         return min(dp[n-1],dp[n-2]);
         
     }
+    
+    int solve4(vector<int>& cost,int n){
+        int prev1=cost[1];
+        int prev2=cost[0];
+        
+        for(int i=2;i<n;i++){
+            int curr=cost[i]+min(prev1,prev2);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return min(prev1,prev2);
+    }
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
         
         
-        int ans=solve3(cost,n);
+        int ans=solve4(cost,n);
         return ans;
     }
 };
