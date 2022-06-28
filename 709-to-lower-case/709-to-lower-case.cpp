8bcb96555f3fdd56dc112145d1c65201a08e715d
Pawan kumar sah <53905882+pawan8893@users.noreply.github.com>
2022-06-28 17:13:24 +0530

    Time: 3 ms (42.23%), Space: 6.1 MB (81.79%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        
+        int n=s.length();
+        
+        for(int i=0;s[i]!='\0';i++){
+            if(s[i]>= 'A' && s[i] <='Z'){
+                s[i]=s[i]+32;
+            }
+        }
+        return s;
+        
+    }
+};
