1893d975d801e8682a68efcda7dbf8e2b9a34af0
Pawan kumar sah <53905882+pawan8893@users.noreply.github.com>
2022-06-30 00:44:21 +0530

    Time: 9 ms (33.03%), Space: 9.3 MB (48.84%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -2,6 +2,16 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         
+        /*
+             Algo
+             
+             step 1---We first sort the array of strings.
+             step 2---Then, we choose the first and last string in the array. [They are supposed to be the most different among                all the pairs of strings in the sorted array]
+             step 3---We just compare how many common characters match from index i = 0 of these two strings.
+        
+        
+        */
+        
         int n=strs.size();
         sort(strs.begin(),strs.end());
         string a=strs[0];
