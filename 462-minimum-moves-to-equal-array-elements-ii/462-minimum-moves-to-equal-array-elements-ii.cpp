3a0fc4ab495bdd1df26794b0d6d553cb28996d4c
Pawan kumar sah <53905882+pawan8893@users.noreply.github.com>
2022-07-01 00:30:31 +0530

    Time: 11 ms (86.97%), Space: 10.9 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -6,6 +6,23 @@ public:
         
         int cnt=0;
         
+        
+        for(int i=0;i<n/2;i++){
+            cnt += abs(nums[i]-nums[n-i-1]);
+        }
+        return cnt;
+    }
+};
+
+/*
+    Approach 1
+    
+     int minMoves2(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        
+        int cnt=0;
+        
         int median =nums[n/2];
         
         for(int i=0;i<n;i++){
@@ -13,4 +30,16 @@ public:
         }
         return cnt;
     }
-};
+
+
+
+
+
+
+
+
+
+
+
+
+*/
