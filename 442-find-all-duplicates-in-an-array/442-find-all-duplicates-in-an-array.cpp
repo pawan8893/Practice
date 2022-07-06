a8692af1265c56b0e6ed10b73f0680fb065953d0
Pawan kumar sah <53905882+pawan8893@users.noreply.github.com>
2022-07-06 22:33:13 +0530

    Time: 143 ms (21.00%), Space: 33.7 MB (48.82%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -4,14 +4,12 @@ public:
         
         vector<int> ans;
         sort(nums.begin(),nums.end());
-        int n=nums.size();
         
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<nums.size()-1;i++){
             if(nums[i]==nums[i+1]){
                 ans.push_back(nums[i]);
             }
         }
-        
         return ans;
     }
 };
