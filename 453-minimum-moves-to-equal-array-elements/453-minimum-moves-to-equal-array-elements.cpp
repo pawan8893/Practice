bd2323c8c84970c0b6bfbb5b9b3aecc31d339fb2
Pawan kumar sah <53905882+pawan8893@users.noreply.github.com>
2022-07-09 23:32:30 +0530

    Time: 54 ms (71.29%), Space: 28.3 MB (83.20%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
+        
+        
+        /*   
+            as decre   APPROACH
+               
+               Incrementing n-1 element by 1 will be same as  decrementing all the elements by 1 in each move until they are                    equal to the minimum element in the array.
+               
+                For example- [5,6,7,8] can be made equal if we decrement number 8, three times and 7, two times and 6 one times.                 Decrementing 8 by 1 is equal to adding 1 to all the other elements.
+                Conclusion - We are just reversing the problem to make it simpler :)
+        
+        
+        
+        */
+        
+        
+        
         int mini=INT_MAX;
         int moves=0;
         
