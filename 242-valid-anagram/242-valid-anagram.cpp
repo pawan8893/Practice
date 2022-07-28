7bdb4f19196a670020ffb5d7d1d1d0e5513b27e6
Pawan kumar sah <53905882+pawan8893@users.noreply.github.com>
2022-07-29 01:29:32 +0530

    Time: 12 ms (65.55%), Space: 7.2 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+       
+        if(s.length()!=t.length()){
+            return false;
+        }
         
-        return s==t;
+        unordered_map<char,int> count;
+        
+        for(int i=0;i<s.length();i++){
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+        
+        for(auto it : count){
+            if(it.second){
+                return false;
+            }
+        }
+        return true;
     }
 };
