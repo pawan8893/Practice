0dd35c90c63259fa459a9b7e58c509f6d39d5858
Pawan kumar sah <53905882+pawan8893@users.noreply.github.com>
2022-08-03 18:20:55 +0530

    Time: 35 ms (37.68%), Space: 19.6 MB (13.21%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> count;
+        int n=nums.size();
+        int len=n/2;
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+            count[nums[i]]++;
+        }
+        
+        for(auto it:count){
+            if(it.second>len){
+                ans=it.first;
+            }
+        }
+        return ans;
+    }
+};
+
+
+/*
+
+       TLE Approach -- TC--> O(n^2)+O(nlogn)
+       
+       int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int len=n/2;
+        sort(nums.begin(),nums.end());
+        int count=0;
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+            count=0;
+            for(int j=i;j<n;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+                if(count>len){
+                        ans=nums[i];
+                }
+            }
+        }
+        return ans;
+    }
+
+
+*/
