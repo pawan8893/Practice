b9561bc808ee44582e19f25edc7ce3398359a722
Pawan kumar sah <53905882+pawan8893@users.noreply.github.com>
2022-09-25 01:28:15 +0530

    Time: 3 ms (62.36%), Space: 7.2 MB (5.46%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -20,3 +20,63 @@ public:
         return st.top().size();
     }
 };
+
+
+/*
+
+2nd Approach
+
+
+class Solution {
+    string reverse(string s){
+        stack<string> st;
+        string ans;
+        
+        for(int i=0;i<s.size();i++){
+            string word;
+            if(s[i]==' '){
+                continue;
+            }
+            
+            while(i<s.size() && s[i]!=' '){
+                word +=s[i];
+                i++;
+            }
+            
+            st.push(word);
+        }
+        
+        while(!st.empty()){
+            ans +=st.top();
+            st.pop();
+            
+            if(!st.empty()){
+                ans +=' ';
+            }
+        }
+        
+        return ans;
+    }
+public:
+    int lengthOfLastWord(string s) {
+        if(s.size()==0){
+            return 0;
+        }
+        
+        int count=0;
+        
+        string str=reverse(s);
+        
+        for(int i=0;i<str.size();i++){
+            if(str[i]==' '){
+                break;
+            }
+            else{
+                count +=1;
+            }
+        }
+        return count;
+    }
+};
+
+*/
